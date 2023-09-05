c22d9215505a885e24ec4e1462eb8dacf82dc080
Ripon <ripon18922@gmail.com>
2023-09-06 02:22:26 +0600

    show details button and enter button functionality done

--- js/app.js
+++ js/app.js
@@ -40,6 +40,7 @@ noPhone.classList.remove('d-none')
             <h5 class="card-title">${phone.phone_name}</h5>
             <p class="card-text">This is a longer card with supporting text below as a natural
                 lead-in to additional content. This content is a little bit longer.</p>
+                <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary">Show Details</button>
         </div>
     </div>
     `;
@@ -66,6 +67,14 @@ document.getElementById('btn-search').addEventListener('click', function(){
     processSearch(10);
 })
 
+// search input field enter key handler 
+document.getElementById('search-field').addEventListener('keypress', function(e){
+    console.log(e.key);
+    if(e.key === 'Enter'){
+        processSearch(10);
+    }
+})
+
 const toggleSpinner = isLoading =>{
 const loaderSection = document.getElementById('loader');
 if(isLoading){
@@ -80,5 +89,13 @@ if(isLoading){
 document.getElementById('btn-show-all').addEventListener('click', function(){
     processSearch();
 })
+
+
+const loadPhoneDetails = async id =>{
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.data);
+}
 
 // loadPhones();
